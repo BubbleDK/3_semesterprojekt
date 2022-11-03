ba9b4677146d662cb25dc77aba3fe76e98f4b608
fundish <93514755+fundish@users.noreply.github.com>
2022-11-03 12:39:41 +0100

    gamingstation

---
+++ NetCafeUCN/DataAccessLayer/Model/GamingStation.cs
@@ -0,0 +1,26 @@
+﻿using DataAccessLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCafeUCN.DAL.Model
+{
+    public class GamingStation : Product
+    {
+        public string SeatNumber { get; set; }
+        public bool Booked  { get; set; }
+        public int Tier { get; set; }
+        public GamingStation(string productNumber, string description, string seatNumber, int Tier) : base(productNumber, description)
+        {
+            this.ProductNumber = productNumber;
+            this.Description = description;
+            this.SeatNumber = seatNumber;
+            this.Tier = Tier;
+            this.Booked = false;
+
+        }
+
+    }
+}
