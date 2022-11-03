53e10acda65b0c7ad91fd9e5f25848e988c0e1e1
MADUCN <93515405+MADUCN@users.noreply.github.com>
2022-11-03 09:38:08 +0100

    UserDAO GetAll started

--- NetCafeUCN/DataAccessLayer/UserDataAccess.cs
+++ NetCafeUCN/DataAccessLayer/UserDataAccess.cs
@@ -4,11 +4,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.ComponentModel.DataAnnotations;
 
 namespace DataAccessLayer
 {
     public class UserDataAccess : INetCafeDataAccess<Person>
     {
+        private string connectionString = @"Data Source=hildur.ucn.dk;User ID=DMA-CSD-S212_10182474;Password=Password1!;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
         public int Add(Person o)
         {
             throw new NotImplementedException();
@@ -21,7 +24,34 @@ namespace DataAccessLayer
 
         public IEnumerable<Person> GetAll()
         {
-            throw new NotImplementedException();
+            string sqlStatement = "SELECT * FROM Persons";
+            List<Person> list = new List<Person>();
+            using(SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand(sqlStatement, conn);
+
+                try
+                {
+                    conn.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Person person = new Customer()
+                        {
+                            //name = (string)reader["name"],
+                            //phone = (string)reader["phone"],
+                            //email = (string)reader["email"]
+                        };
+                        list.Add(person);
+                    }
+                }
+                catch
+                {
+
+                    throw;
+                }
+            }
+            return list;
         }
 
         public bool Remove(int id)
