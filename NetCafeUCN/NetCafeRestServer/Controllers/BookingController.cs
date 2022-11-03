69139bf6f759ecd52200861a246f21c296b992a5
fundish <93514755+fundish@users.noreply.github.com>
2022-11-03 10:41:25 +0100

    api

---
+++ NetCafeUCN/NetCafeRestServer/Controllers/BookingController.cs
@@ -0,0 +1,46 @@
+﻿using DataAccessLayer.DAO;
+using DataAccessLayer.Model;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace NetCafeRestServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingController : ControllerBase
+    {
+        private INetCafeDataAccess<Booking> dataAccess;
+        // GET: api/<BookingController>
+        [HttpGet]
+        public IEnumerable<Booking> Get()
+        {
+            return Ok(dataAccess.GetAll());
+        }
+
+        // GET api/<BookingController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<BookingController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<BookingController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<BookingController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
