365039c07e3cf359a3ee0bad6b0b56999eb62aab
fundish <93514755+fundish@users.noreply.github.com>
2022-11-03 11:49:56 +0100

    person controller

---
+++ NetCafeUCN/NetCafeRestServer/Controllers/PersonController.cs
@@ -0,0 +1,67 @@
+﻿using DataAccessLayer.DAO;
+using DataAccessLayer.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NetCafeUCN.API.Controllers
+{
+    namespace NetCafeRestServer.Controllers
+    {
+        [Route("api/[controller]")]
+        [ApiController]
+        public class PersonController : ControllerBase
+        {
+            private INetCafeDataAccess<Person> dataAccess;
+
+            public PersonController(INetCafeDataAccess<Person> dataAccess)
+            {
+                this.dataAccess = dataAccess;
+            }
+
+            // GET: api/<PersonController>
+            [HttpGet]
+            public ActionResult<IEnumerable<Person>> Get()
+            {
+                return Ok(dataAccess.GetAll());
+            }
+
+            // GET api/<PersonController>/40559810
+            [HttpGet]
+            [Route("{phoneNo}")]
+            public ActionResult<Person> Get(string phoneNo)
+            {
+                var person = dataAccess.Get(phoneNo);
+                if (person == null) { return NotFound(); }
+                return Ok(person);
+            }
+
+            // POST api/<PersonController>
+            [HttpPost]
+            public ActionResult<int> Add(Person person)
+            {
+                return Ok(dataAccess.Add(person));
+            }
+
+            // PUT api/<PersonController>/
+            [HttpPut]
+            public ActionResult<bool> Update(Person person)
+            {
+                return Ok(dataAccess.Update(person));
+            }
+
+            // DELETE api/<PersonController>/40559810
+            [HttpDelete("{phoneNo}")]
+            public ActionResult<bool> Delete(int phoneNo)
+            {
+                if (dataAccess.Remove(phoneNo))
+                {
+                    return Ok();
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+        }
+    }
+}
