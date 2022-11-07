8fc0fece0cc148f7fd7d4fe2bb7947004bebe0d4
fundish <93514755+fundish@users.noreply.github.com>
2022-11-07 09:57:50 +0100

    Update Employee.cs

--- NetCafeUCN/DataAccessLayer/Model/Employee.cs
+++ NetCafeUCN/DataAccessLayer/Model/Employee.cs
@@ -9,7 +9,6 @@ namespace DataAccessLayer.Model
     internal class Employee:Person
     {
         public string Role { get; set; }
-        public string Access { get; set; }
         public string Address { get; set; }
         public int Zipcode { get; set; }
         public string City { get; set; }
