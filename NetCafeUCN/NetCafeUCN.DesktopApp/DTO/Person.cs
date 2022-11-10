4118caa80038a40fd2a60803da9c5201e123a11a
RGUCN <10182474@ucn.dk>
2022-11-10 11:42:09 +0100

    tilføjet cinstructor til DTO Person

--- NetCafeUCN/NetCafeUCN.DesktopApp/DTO/Person.cs
+++ NetCafeUCN/NetCafeUCN.DesktopApp/DTO/Person.cs
@@ -11,7 +11,14 @@ namespace NetCafeUCN.DesktopApp.DTO
         public string? Name { get; set; }
         public string? Email { get; set; }
         public string? Phone { get; set; }
-
+        public string PersonType { get; set; }
+        public Person(string name, string email, string phone, string personType)
+        {
+            Name = name;
+            Email = email;
+            Phone = phone;
+            PersonType = personType;
+        }
         public override string ToString()
         {
             return Name;
