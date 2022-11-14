2b3c194ca3784d9483621bc081dd2fd59ee0cb50
JakobUCN <93514767+JakobUCN@users.noreply.github.com>
2022-11-14 11:22:52 +0100

    Nye GUI vinduer

---
+++ NetCafeUCN/NetCafeUCN.DesktopApp/InputDialog.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NetCafeUCN.DesktopApp
+{
+    public partial class InputDialog : Form
+    {
+        public InputDialog(string inputFormIdentity)
+        {
+            InitializeComponent();
+            lblInput.Text = inputFormIdentity;
+        }
+
+        private void btnConfirm_Click(object sender, EventArgs e)
+        {
+            confirmInupt();
+        }
+
+        private void confirmInupt()
+        {
+            if (txtInput.Text.ToLower().Equals("gamingstation"))
+            {
+                Form gamingStationForm = new GamingstationForm();
+                gamingStationForm.Show();
+            }
+            else if (txtInput.Text.ToLower().Equals("consumable"))
+            {
+
+            }
+        }
+    }
+}
