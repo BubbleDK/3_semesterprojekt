0d706360204674cbad94cfc0e458035925c2bab8
Bubble <emiltols2001@outlook.dk>
2022-11-14 11:42:40 +0100

    Add singleton program.cs

--- NetCafeUCN/NetCafeRestServer/Program.cs
+++ NetCafeUCN/NetCafeRestServer/Program.cs
@@ -9,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton<INetCafeDataAccess<Booking>, BookingDataAccess>();
 builder.Services.AddSingleton<INetCafeDataAccess<Person>, UserDataAccess>();
 builder.Services.AddSingleton<INetCafeDataAccess<Product>, ProductDataAccess>();
+builder.Services.AddSingleton<INetCafeDataAccess<Consumable>, ConsumableDataAccess>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
