7605a4222be4b2a9521d6aeb57fc2e3acf938ee7
fundish <93514755+fundish@users.noreply.github.com>
2022-11-14 10:08:17 +0100

    generic product ctrl api

---
+++ NetCafeUCN/NetCafeRestServer/Controllers/GenericProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCafeUCN.API.Controllers
+{
+    public class GenericProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
