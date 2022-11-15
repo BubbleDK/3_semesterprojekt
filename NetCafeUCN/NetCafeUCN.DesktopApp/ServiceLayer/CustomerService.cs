9bae482b922736b23532acaaea9933a2d40d0567
Rasmus <10182474@ucn.dk>
2022-11-15 10:41:16 +0100

    CustomerService metoder tilføjet med funktionalitet

---
+++ NetCafeUCN/NetCafeUCN.DesktopApp/ServiceLayer/CustomerService.cs
@@ -0,0 +1,45 @@
+﻿using NetCafeUCN.DesktopApp.DTO;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCafeUCN.DesktopApp.ServiceLayer
+{
+    internal class CustomerService : INetCafeDataAccess<Customer>
+    {
+        public string BaseUri { get; private set; }
+        private RestClient RestClient { get; set; }
+        public CustomerService(string baseUri)
+        {
+            BaseUri = baseUri;
+            RestClient = new RestClient();
+        }
+        public bool Add(Customer o)
+        {
+            return RestClient.Execute<Customer>(new RestRequest($"{BaseUri}/{o}", Method.Post)).IsSuccessful;
+        }
+
+        public Customer? Get(dynamic key)
+        {
+            return RestClient.Execute<Customer>(new RestRequest($"{BaseUri}/{key}", Method.Get)).Data;
+        }
+
+        public IEnumerable<Customer> GetAll()
+        {
+            return RestClient.Execute<IEnumerable<Customer>>(new RestRequest()).Data;
+        }
+
+        public bool Remove(dynamic key)
+        {
+            return RestClient.Execute<Customer>(new RestRequest($"{BaseUri}/{key}", Method.Delete)).IsSuccessful;
+        }
+
+        public bool Update(Customer o)
+        {
+            return RestClient.Execute<Customer>(new RestRequest($"{BaseUri}/{o}", Method.Put)).IsSuccessful;
+        }
+    }
+}
