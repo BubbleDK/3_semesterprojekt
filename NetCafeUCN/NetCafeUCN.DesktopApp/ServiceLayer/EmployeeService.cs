cb4a3f8833d89288ea750257ade0dc1fe52eff64
Rasmus <10182474@ucn.dk>
2022-11-15 10:46:51 +0100

    Tilføjet funktionalitet til EmployeeService

---
+++ NetCafeUCN/NetCafeUCN.DesktopApp/ServiceLayer/EmployeeService.cs
@@ -0,0 +1,45 @@
+﻿using NetCafeUCN.DesktopApp.DTO;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCafeUCN.DesktopApp.ServiceLayer
+{
+    internal class EmployeeService : INetCafeDataAccess<Employee>
+    {
+        public string BaseUri { get; private set; }
+        private RestClient RestClient { get; set; }
+        public EmployeeService(string baseUri)
+        {
+            BaseUri = baseUri;
+            RestClient = new RestClient();
+        }
+        public bool Add(Employee o)
+        {
+            return RestClient.Execute<Employee>(new RestRequest($"{BaseUri}/{o}",Method.Post)).IsSuccessful;
+        }
+
+        public Employee? Get(dynamic key)
+        {
+            return RestClient.Execute<Employee>(new RestRequest($"{BaseUri}/{key}",Method.Get)).Data;
+        }
+
+        public IEnumerable<Employee> GetAll()
+        {
+            return RestClient.Execute<IEnumerable<Employee>>(new RestRequest()).Data;
+        }
+
+        public bool Remove(dynamic key)
+        {
+            return RestClient.Execute<Employee>(new RestRequest($"{BaseUri}/{key}", Method.Delete)).IsSuccessful;
+        }
+
+        public bool Update(Employee o)
+        {
+            return RestClient.Execute<Employee>(new RestRequest($"{BaseUri}/{o}",Method.Put)).IsSuccessful;
+        }
+    }
+}
