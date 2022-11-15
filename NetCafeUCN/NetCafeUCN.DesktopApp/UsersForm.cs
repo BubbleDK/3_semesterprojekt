437493957d5c2f1f84a3a5501fb29a3c398872cf
Rasmus <10182474@ucn.dk>
2022-11-15 10:48:41 +0100

    Fjernet instantiering af BaseUri

--- NetCafeUCN/NetCafeUCN.DesktopApp/UsersForm.cs
+++ NetCafeUCN/NetCafeUCN.DesktopApp/UsersForm.cs
@@ -5,7 +5,7 @@ namespace NetCafeUCN.DesktopApp
 {
     public partial class UsersForm : Form
     {
-        INetCafeDataAccess<Customer> CustomerService = new CustomerService("https://localhost:7197/api/Customer");
+        
         public UsersForm()
         {
             InitializeComponent();
