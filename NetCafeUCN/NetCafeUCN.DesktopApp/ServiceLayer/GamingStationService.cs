6b5fe6d3d69738448428b3daed0e480c621a0de0
RGUCN <10182474@ucn.dk>
2022-11-17 10:00:05 +0100

    Tilføjet ConsumableService

---
+++ NetCafeUCN/NetCafeUCN.DesktopApp/ServiceLayer/GamingStationService.cs
@@ -0,0 +1,45 @@
+﻿using NetCafeUCN.DesktopApp.DTO;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCafeUCN.DesktopApp.ServiceLayer
+{
+    internal class GamingStationService : INetCafeDataAccess<GamingStation>
+    {
+        public string BaseUri { get; private set; }
+        private RestClient RestClient { get; set; }
+        public GamingStationService(string baseUri)
+        {
+            BaseUri = baseUri;
+            RestClient = new RestClient();
+        }
+        public bool Add(GamingStation o)
+        {
+            throw new NotImplementedException();
+        }
+
+        public GamingStation? Get(dynamic key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<GamingStation> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(dynamic key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(GamingStation o)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
