657aa467cc98ccfc66387c1ef169eab7a4e23b4b
JakobUCN <10407505@ucn.dk>
2022-11-22 09:56:53 +0100

    Changed gamingstationDTO in desktop from booked to isactive

--- NetCafeUCN/NetCafeUCN.DesktopApp/BookingForms/NewBookingForm.Designer.cs
+++ NetCafeUCN/NetCafeUCN.DesktopApp/BookingForms/NewBookingForm.Designer.cs
@@ -51,6 +51,9 @@
             // 
             // dateTimePicker1
             // 
+            this.dateTimePicker1.Format = DateTimePickerFormat.Custom;
+            this.dateTimePicker1.CustomFormat = "dd/MM/yyyy hh:mm";
+            this.dateTimePicker1.ShowUpDown = true;
             this.dateTimePicker1.Location = new System.Drawing.Point(3, 3);
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.Size = new System.Drawing.Size(235, 23);
