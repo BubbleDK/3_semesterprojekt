bdf602c19972a1f6886c886997263dbd1f16b6fd
Jkjeldsteen <10407505@ucn.dk>
2022-11-24 11:43:47 +0100

    Jeg prøver at filtrere gamingstations ud fra valgte tidspunkter

--- NetCafeUCN/NetCafeUCN.DesktopApp/BookingForms/NewBookingForm.Designer.cs
+++ NetCafeUCN/NetCafeUCN.DesktopApp/BookingForms/NewBookingForm.Designer.cs
@@ -36,8 +36,8 @@
             this.lblEndTime = new System.Windows.Forms.Label();
             this.lblStartTime = new System.Windows.Forms.Label();
             this.cmbStartTime = new System.Windows.Forms.ComboBox();
-            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.clndPicker = new System.Windows.Forms.MonthCalendar();
+            this.dgvAvailableGamingstations = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
@@ -46,14 +46,13 @@
             this.splitContainer2.Panel1.SuspendLayout();
             this.splitContainer2.Panel2.SuspendLayout();
             this.splitContainer2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAvailableGamingstations)).BeginInit();
             this.SuspendLayout();
             // 
             // splitContainer1
             // 
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.splitContainer1.Location = new System.Drawing.Point(0, 0);
-            this.splitContainer1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.splitContainer1.Name = "splitContainer1";
             // 
             // splitContainer1.Panel1
@@ -62,17 +61,15 @@
             // 
             // splitContainer1.Panel2
             // 
-            this.splitContainer1.Panel2.Controls.Add(this.dataGridView1);
-            this.splitContainer1.Size = new System.Drawing.Size(1225, 805);
-            this.splitContainer1.SplitterDistance = 319;
-            this.splitContainer1.SplitterWidth = 5;
+            this.splitContainer1.Panel2.Controls.Add(this.dgvAvailableGamingstations);
+            this.splitContainer1.Size = new System.Drawing.Size(1072, 604);
+            this.splitContainer1.SplitterDistance = 279;
             this.splitContainer1.TabIndex = 0;
             // 
             // splitContainer2
             // 
             this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.splitContainer2.Location = new System.Drawing.Point(0, 0);
-            this.splitContainer2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.splitContainer2.Name = "splitContainer2";
             this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
             // 
@@ -87,56 +84,54 @@
             this.splitContainer2.Panel2.Controls.Add(this.lblEndTime);
             this.splitContainer2.Panel2.Controls.Add(this.lblStartTime);
             this.splitContainer2.Panel2.Controls.Add(this.cmbStartTime);
-            this.splitContainer2.Panel2.Controls.Add(this.monthCalendar1);
-            this.splitContainer2.Size = new System.Drawing.Size(319, 805);
-            this.splitContainer2.SplitterDistance = 98;
-            this.splitContainer2.SplitterWidth = 5;
+            this.splitContainer2.Panel2.Controls.Add(this.clndPicker);
+            this.splitContainer2.Size = new System.Drawing.Size(279, 604);
+            this.splitContainer2.SplitterDistance = 73;
             this.splitContainer2.TabIndex = 0;
             // 
             // textBox2
             // 
             this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox2.Location = new System.Drawing.Point(153, 28);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox2.Location = new System.Drawing.Point(134, 21);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(131, 27);
+            this.textBox2.Size = new System.Drawing.Size(115, 23);
             this.textBox2.TabIndex = 3;
             // 
             // lblPhone
             // 
             this.lblPhone.AutoSize = true;
-            this.lblPhone.Location = new System.Drawing.Point(14, 32);
+            this.lblPhone.Location = new System.Drawing.Point(12, 24);
             this.lblPhone.Name = "lblPhone";
-            this.lblPhone.Size = new System.Drawing.Size(58, 20);
+            this.lblPhone.Size = new System.Drawing.Size(45, 15);
             this.lblPhone.TabIndex = 1;
             this.lblPhone.Text = "Telefon";
             // 
             // cmbEndTime
             // 
+            this.cmbEndTime.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.cmbEndTime.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbEndTime.FormattingEnabled = true;
-            this.cmbEndTime.Location = new System.Drawing.Point(14, 361);
-            this.cmbEndTime.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.cmbEndTime.Location = new System.Drawing.Point(12, 271);
             this.cmbEndTime.Name = "cmbEndTime";
-            this.cmbEndTime.Size = new System.Drawing.Size(257, 28);
+            this.cmbEndTime.Size = new System.Drawing.Size(225, 23);
             this.cmbEndTime.TabIndex = 4;
-            this.cmbEndTime.SelectedIndexChanged += new System.EventHandler(this.cmbEndTime_SelectedIndexChanged);
+            this.cmbEndTime.SelectionChangeCommitted += new System.EventHandler(this.cmbEndTime_SelectionChangeCommitted);
             // 
             // lblEndTime
             // 
             this.lblEndTime.AutoSize = true;
-            this.lblEndTime.Location = new System.Drawing.Point(14, 337);
+            this.lblEndTime.Location = new System.Drawing.Point(12, 253);
             this.lblEndTime.Name = "lblEndTime";
-            this.lblEndTime.Size = new System.Drawing.Size(52, 20);
+            this.lblEndTime.Size = new System.Drawing.Size(41, 15);
             this.lblEndTime.TabIndex = 3;
             this.lblEndTime.Text = "Sluttid";
             // 
             // lblStartTime
             // 
             this.lblStartTime.AutoSize = true;
-            this.lblStartTime.Location = new System.Drawing.Point(14, 240);
+            this.lblStartTime.Location = new System.Drawing.Point(12, 180);
             this.lblStartTime.Name = "lblStartTime";
-            this.lblStartTime.Size = new System.Drawing.Size(61, 20);
+            this.lblStartTime.Size = new System.Drawing.Size(48, 15);
             this.lblStartTime.TabIndex = 2;
             this.lblStartTime.Text = "Starttid:";
             // 
@@ -145,39 +140,41 @@
             this.cmbStartTime.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.cmbStartTime.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbStartTime.FormattingEnabled = true;
-            this.cmbStartTime.Location = new System.Drawing.Point(12, 264);
-            this.cmbStartTime.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.cmbStartTime.Location = new System.Drawing.Point(10, 198);
             this.cmbStartTime.Name = "cmbStartTime";
-            this.cmbStartTime.Size = new System.Drawing.Size(258, 28);
+            this.cmbStartTime.Size = new System.Drawing.Size(226, 23);
             this.cmbStartTime.TabIndex = 1;
-            this.cmbStartTime.SelectedIndexChanged += new System.EventHandler(this.cmbStartTime_SelectedIndexChanged);
+            this.cmbStartTime.SelectionChangeCommitted += new System.EventHandler(this.cmbStartTime_SelectionChangeCommitted);
             // 
-            // monthCalendar1
+            // clndPicker
             // 
-            this.monthCalendar1.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.monthCalendar1.Location = new System.Drawing.Point(12, 12);
-            this.monthCalendar1.Margin = new System.Windows.Forms.Padding(10, 12, 10, 12);
-            this.monthCalendar1.Name = "monthCalendar1";
-            this.monthCalendar1.TabIndex = 0;
+            this.clndPicker.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.clndPicker.Location = new System.Drawing.Point(10, 9);
+            this.clndPicker.MaxSelectionCount = 1;
+            this.clndPicker.MinDate = new System.DateTime(1971, 1, 1, 0, 0, 0, 0);
+            this.clndPicker.Name = "clndPicker";
+            this.clndPicker.TabIndex = 0;
             // 
-            // dataGridView1
+            // dgvAvailableGamingstations
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 29;
-            this.dataGridView1.Size = new System.Drawing.Size(901, 805);
-            this.dataGridView1.TabIndex = 0;
+            this.dgvAvailableGamingstations.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAvailableGamingstations.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvAvailableGamingstations.Location = new System.Drawing.Point(0, 0);
+            this.dgvAvailableGamingstations.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dgvAvailableGamingstations.Name = "dgvAvailableGamingstations";
+            this.dgvAvailableGamingstations.ReadOnly = true;
+            this.dgvAvailableGamingstations.RowHeadersWidth = 51;
+            this.dgvAvailableGamingstations.RowTemplate.Height = 29;
+            this.dgvAvailableGamingstations.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvAvailableGamingstations.Size = new System.Drawing.Size(789, 604);
+            this.dgvAvailableGamingstations.TabIndex = 0;
             // 
             // NewBookingForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1225, 805);
+            this.ClientSize = new System.Drawing.Size(1072, 604);
             this.Controls.Add(this.splitContainer1);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "NewBookingForm";
             this.Text = "NewBookingForm";
             this.splitContainer1.Panel1.ResumeLayout(false);
@@ -190,7 +187,7 @@
             this.splitContainer2.Panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
             this.splitContainer2.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAvailableGamingstations)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -205,7 +202,7 @@
         private Label lblEndTime;
         private Label lblStartTime;
         private ComboBox cmbStartTime;
-        private MonthCalendar monthCalendar1;
-        private DataGridView dataGridView1;
+        private MonthCalendar clndPicker;
+        private DataGridView dgvAvailableGamingstations;
     }
 }
