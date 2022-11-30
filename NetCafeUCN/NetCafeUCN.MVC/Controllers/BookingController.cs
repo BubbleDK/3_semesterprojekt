b284cdd05e58767fd34567e077e0ed46e2b3318f
Bubble <emiltols2001@outlook.dk>
2022-11-30 13:27:40 +0100

    set values for forms

--- NetCafeUCN/NetCafeUCN.MVC/Controllers/BookingController.cs
+++ NetCafeUCN/NetCafeUCN.MVC/Controllers/BookingController.cs
@@ -32,6 +32,8 @@ namespace NetCafeUCN.MVC.Controllers
             BookingGamingStationViewModel viewModel = new BookingGamingStationViewModel();
             viewModel.gamingStations = gamingStationService.GetAll();
 
+
+
             return View(viewModel);
         }
 
