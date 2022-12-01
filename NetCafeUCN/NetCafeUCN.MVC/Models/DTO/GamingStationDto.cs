17d655cffb80984bb5d09f1cfc743afa85e809a6
Bubble <emiltols2001@outlook.dk>
2022-12-01 10:40:18 +0100

    Create booking mvc progress

--- NetCafeUCN/NetCafeUCN.MVC/Models/DTO/GamingStationDto.cs
+++ NetCafeUCN/NetCafeUCN.MVC/Models/DTO/GamingStationDto.cs
@@ -10,5 +10,6 @@ namespace NetCafeUCN.MVC.Models
     {
         public string? SeatNumber { get; set; }
         public string? Description { get; set; }
+        public bool Booked { get; set; }
     }
 }
---
+++ NetCafeUCN/NetCafeUCN.MVC/wwwroot/js/CreateBooking.js
@@ -0,0 +1,10 @@
+﻿//document.querySelector("#testInput").addEventListener("input", test);
+
+//function test() {
+//    console.log("Jeg er blevet opdateret!")
+//}
+
+let today = new Date();
+console.log(today);
+
+document.getElementById('testInput').value = today;
