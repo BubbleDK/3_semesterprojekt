9297ef17bfd9bbd7f0828dd1eceb3051b162846f
fundish <93514755+fundish@users.noreply.github.com>
2022-12-08 09:24:58 +0100

    Fisx af phoneNo mvc

--- NetCafeUCN/NetCafeUCN.MVC/Models/DTO/BookingDto.cs
+++ NetCafeUCN/NetCafeUCN.MVC/Models/DTO/BookingDto.cs
@@ -1,9 +1,11 @@
 ﻿using NetCafeUCN.MVC.Models.DTO;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace NetCafeUCN.MVC.Models
 {
@@ -12,6 +14,10 @@ namespace NetCafeUCN.MVC.Models
         public string? BookingNo { get; set; }
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
+        [Required(ErrorMessage = "Skriv et gyldigt telefonnummer")]
+        [Display(Name = "Phone number")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression("^\\+?[1-9][0-9]{7}$", ErrorMessage = "Ikke et gyldigt telefonnummer")]
         public string PhoneNo { get; set; }
         public List<BookingLineDto> BookingLines { get; set; }
         public BookingDto()
