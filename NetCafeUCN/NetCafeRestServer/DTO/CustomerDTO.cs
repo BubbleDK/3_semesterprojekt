e727b34fdf5ae7bb512693c5d7bf6ec0fa1c2564
Bubble <49866759+BubbleDK@users.noreply.github.com>
2022-12-18 13:56:36 +0100

    Documentation CustomerDTO

--- NetCafeUCN/NetCafeRestServer/DTO/CustomerDTO.cs
+++ NetCafeUCN/NetCafeRestServer/DTO/CustomerDTO.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace NetCafeUCN.API.DTO
 {
+    /// <summary>
+    ///  CustomerDTO model, arver fra PersonDTO
+    /// </summary>
     public class CustomerDTO : PersonDTO
     {
     }
