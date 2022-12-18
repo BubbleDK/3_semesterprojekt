5c382153aa2e9411e2afb2ef5fe9c6ca76753556
Bubble <49866759+BubbleDK@users.noreply.github.com>
2022-12-18 13:49:58 +0100

    Documentation BookingLineToDtoConversion

--- NetCafeUCN/NetCafeRestServer/Conversion/BookingLineToDtoConversion.cs
+++ NetCafeUCN/NetCafeRestServer/Conversion/BookingLineToDtoConversion.cs
@@ -3,8 +3,15 @@ using NetCafeUCN.DAL.Model;
 
 namespace NetCafeUCN.API.Conversion
 {
+    /// <summary>
+    ///  BookingLineToDtoConversion klassen er statisk og bruges til at konvertere bookinglines til DTO'er
+    /// </summary>
     public static class BookingLineToDtoConversion
     {
+        /// <summary>
+        /// Statisk metode som konvertere en collection af BookingLine til BookingLineDTO
+        /// </summary>
+        /// <param name="bookinglines">Collection af bookinglinjer</param>
         public static IEnumerable<BookingLineDTO> BookingLineToDtos(this IEnumerable<BookingLine> bookinglines)
         {
             foreach (var bookingline in bookinglines)
@@ -12,6 +19,11 @@ namespace NetCafeUCN.API.Conversion
                 yield return bookingline.BookingLineToDto();
             }
         }
+        /// <summary>
+        /// Statisk metode som konvertere en BookingLine til BookingLineDTO
+        /// </summary>
+        /// <param name="bookingline">En BookingLine</param>
+        /// <returns>Returnere den konverteret BookingLineDTO</returns>
         public static BookingLineDTO BookingLineToDto(this BookingLine bookingline)
         {
             BookingLineDTO bookingLineDto = bookingline.CopyPropertiesTo(new BookingLineDTO { 
@@ -21,6 +33,10 @@ namespace NetCafeUCN.API.Conversion
             });
             return bookingLineDto;
         }
+        /// <summary>
+        /// Statisk metode som konvertere en collection af BookingLineDTO til BookingLine
+        /// </summary>
+        /// <param name="bookingLineDtos">Collection af BookingLineDTO</param>
         public static IEnumerable<BookingLine> BookingLineFromDtos(this IEnumerable<BookingLineDTO> bookingLineDtos)
         {
             foreach (var bookingline in bookingLineDtos)
@@ -28,7 +44,11 @@ namespace NetCafeUCN.API.Conversion
                 yield return bookingline.BookingLineFromDto();
             }
         }
-
+        /// <summary>
+        /// Statisk metode som konvertere en BookingLineDTO til BookingLine
+        /// </summary>
+        /// <param name="bookingLineDto">En BookingLineDTO</param>
+        /// <returns>Returnere den konverteret BookingLine</returns>
         public static BookingLine BookingLineFromDto(this BookingLineDTO bookingLineDto)
         {
             BookingLine b = bookingLineDto.CopyPropertiesTo(new BookingLine {
