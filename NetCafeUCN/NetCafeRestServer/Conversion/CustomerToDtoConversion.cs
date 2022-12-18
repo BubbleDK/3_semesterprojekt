eae734d72ee9bcc6a2c42f1e5aab820282f079e0
Bubble <49866759+BubbleDK@users.noreply.github.com>
2022-12-18 15:01:23 +0100

    Documentation CustomerToDtoConversion

--- NetCafeUCN/NetCafeRestServer/Conversion/CustomerToDtoConversion.cs
+++ NetCafeUCN/NetCafeRestServer/Conversion/CustomerToDtoConversion.cs
@@ -3,8 +3,15 @@ using NetCafeUCN.DAL.Model;
 
 namespace NetCafeUCN.API.Conversion
 {
+    /// <summary>
+    ///  CustomerToDtoConversion klassen er statisk og bruges til at konvertere Customers
+    /// </summary>
     public static class CustomerToDtoConversion
     {
+        /// <summary>
+        /// Statisk metode som konvertere en collection af Customer til CustomerDTO
+        /// </summary>
+        /// <param name="customers">Collection af Customer</param>
         public static IEnumerable<CustomerDTO> CustomerToDtos(this IEnumerable<Customer> customers)
         {
             foreach (var customer in customers)
@@ -12,10 +19,19 @@ namespace NetCafeUCN.API.Conversion
                 yield return customer.CustomerToDto();
             }
         }
+        /// <summary>
+        /// Statisk metode som konvertere en Customer til CustomerDTO
+        /// </summary>
+        /// <param name="Customer">En Customer</param>
+        /// <returns>Returnere den konverteret CustomerDTO</returns>
         public static CustomerDTO CustomerToDto(this Customer Customer)
         {
             return Customer.CopyPropertiesTo(new CustomerDTO());
         }
+        /// <summary>
+        /// Statisk metode som konvertere en collection af CustomerDTO til Customer
+        /// </summary>
+        /// <param name="customerDtos">Collection af CustomerDTO</param>
         public static IEnumerable<Customer> CustomerFromDtos(this IEnumerable<CustomerDTO> customerDtos)
         {
             foreach (var customer in customerDtos)
@@ -23,7 +39,11 @@ namespace NetCafeUCN.API.Conversion
                 yield return customer.CustomerFromDto();
             }
         }
-
+        /// <summary>
+        /// Statisk metode som konvertere en CustomerDTO til Customer
+        /// </summary>
+        /// <param name="customerDto">En CustomerDTO</param>
+        /// <returns>Returnere den konverteret Customer</returns>
         public static Customer CustomerFromDto(this CustomerDTO customerDto)
         {
             return customerDto.CopyPropertiesTo(new Customer());
