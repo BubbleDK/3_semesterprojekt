557a23a93408a52b9d0fa48544db31ecc6adeee5
MADUCN <93515405+MADUCN@users.noreply.github.com>
2022-12-19 10:09:01 +0100

    Fixed routing in program.cs by adding api/ to base url

--- NetCafeUCN/NetCafeUCN.MVC/Program.cs
+++ NetCafeUCN/NetCafeUCN.MVC/Program.cs
@@ -7,7 +7,7 @@ using NetCafeUCN.MVC.Models.DTO;
 using static System.Net.WebRequestMethods;
 
 var builder = WebApplication.CreateBuilder(args);
-string baseUri = "https://localhost:7197/";
+string baseUri = "https://localhost:7197/api/";
 // Add services to the container.
 builder.Services.AddScoped<IUserProviderService, UserProviderService>(_ => new UserProviderService(baseUri + "Users"));
 builder.Services.AddScoped<INetCafeDataAccessService<GamingStationDto>, GamingstationService>( _ => new GamingstationService(baseUri + "GamingStations"));
